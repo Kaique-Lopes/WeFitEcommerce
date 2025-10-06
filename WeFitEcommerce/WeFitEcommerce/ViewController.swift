d61c50fe91272798f0783e500ef74f6dbe516481
Kaique Lopes <41844709+Kaique-Lopes@users.noreply.github.com>
2025-10-06 18:48:26 -0300

    chore: create ios project

---
+++ WeFitEcommerce/WeFitEcommerce/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeFitEcommerce
+//
+//  Created by Kaique Lopes on 06/10/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
