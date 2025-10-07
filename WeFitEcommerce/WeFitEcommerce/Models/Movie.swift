fa92c6eba8779c3842a2a7e283e751b23b46c27d
Kaique Lopes <41844709+Kaique-Lopes@users.noreply.github.com>
2025-10-07 13:05:14 -0300

    feat: create model/Movie

---
+++ WeFitEcommerce/WeFitEcommerce/Models/Movie.swift
@@ -0,0 +1,15 @@
+//
+//  Movie.swift
+//  WeFitEcommerce
+//
+//  Created by Kaique Lopes on 07/10/25.
+//
+
+import Foundation
+
+struct Movie: Codable {
+    let id: Int
+    let title: String
+    let posterName: String
+    let price: Double
+}
