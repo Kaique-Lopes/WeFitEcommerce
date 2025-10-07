1014d06346f8b0630e2d7e1192aabde06f5e9592
Kaique Lopes <41844709+Kaique-Lopes@users.noreply.github.com>
2025-10-06 21:02:11 -0300

    feat: create Component/PrimaryButton

---
+++ WeFitEcommerce/WeFitEcommerce/Components/PrimaryButton.swift
@@ -0,0 +1,30 @@
+//
+//  PrimaryButton.swift
+//  WeFitEcommerce
+//
+//  Created by Kaique Lopes on 06/10/25.
+//
+
+import UIKit
+
+final class PrimaryButton: UIButton {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        config()
+    }
+    
+    required init?(coder: NSCoder) { fatalError("init(coder:) not implemented") }
+
+    convenience init(title: String) {
+        self.init(frame: .zero)
+        setTitle(title, for: .normal)
+    }
+
+    private func config() {
+        layer.cornerRadius = 8
+        titleLabel?.font = AppFonts.bold(size: 16)
+        backgroundColor = AppColors.primary
+        setTitleColor(.white, for: .normal)
+        heightAnchor.constraint(equalToConstant: 48).isActive = true
+    }
+}
